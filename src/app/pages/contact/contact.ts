f51b88ac007a6ab4862623797a029318c429f8ee
Mayank <mayank@mail.com>
2026-03-19 18:55:27 +0530

    contact form functionality added.

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
   LucideAngularModule,
   Mail,
@@ -12,6 +12,7 @@ import {
 } from 'lucide-angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -32,24 +33,59 @@ export class ContactComponent {
 
   contactForm: FormGroup;
   submitted = false;
+  isSubmitting = false;
+  showSuccess = false;
+
+  private http = inject(HttpClient);
 
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
+      subject: ['', [Validators.required]],
       message: ['', [Validators.required, Validators.minLength(10)]]
     });
   }
 
+  // onSubmit() {
+  //   if (this.contactForm.valid) {
+  //     this.submitted = true;
+  //     console.log('Form Submitted', this.contactForm.value);
+  //     // Reset form after 3 seconds
+  //     setTimeout(() => {
+  //       this.submitted = false;
+  //       this.contactForm.reset();
+  //     }, 3000);
+  //   }
+  // }
+
   onSubmit() {
     if (this.contactForm.valid) {
-      this.submitted = true;
-      console.log('Form Submitted', this.contactForm.value);
-      // Reset form after 3 seconds
-      setTimeout(() => {
-        this.submitted = false;
-        this.contactForm.reset();
-      }, 3000);
+      this.isSubmitting = true;
+
+      console.log(this.contactForm.value);
+      
+      this.http.post('http://localhost:3000/send-email', this.contactForm.value)
+        .subscribe({
+          next: (res) => {
+            this.showSuccess = true;
+            this.isSubmitting = false;
+            this.contactForm.reset();
+          },
+          error: (err) => {
+            console.error('Submission Error:', err);
+            this.isSubmitting = false;
+            alert('Failed to send message. Please try again later.');
+          }
+        });
+    } else {
+      // Mark all fields as touched to trigger validation messages
+      this.contactForm.markAllAsTouched();
     }
+  }
+
+  resetForm() {
+    this.showSuccess = false;
+    this.contactForm.reset();
   }
 }
