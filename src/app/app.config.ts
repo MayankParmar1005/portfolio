dcb0b645ee5e3c37585cc845bbfd73f31f3f430c
MayankParmar1005 <mayankparmar2007@gmail.com>
2026-06-03 16:46:38 +0530

    add google analytic code, in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+// Import the Modules instead of the experimental providers
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient(), provideRouter(routes)],
+  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient(), provideRouter(routes),
+    // Wrap the modules inside importProvidersFrom so they work seamlessly in standalone mode
+    importProvidersFrom(
+      NgxGoogleAnalyticsModule.forRoot('G-7JYYWZRZXH'), // 👈 Replace with your ID
+      NgxGoogleAnalyticsRouterModule.forRoot()
+    )
+  ],
 };
